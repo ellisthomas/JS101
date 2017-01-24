5248028b661a49c38486d2deb5cf9b9dac912906
ellisthomas <ellisthomas@gmail.com>
2017-01-24 17:46:18 -0600

    first commit

---
+++ sonnet.js
@@ -0,0 +1,13 @@
+var element = document.getElementById("sonnet");
+element.innerHTML = sonnet;
+
+var sonnet = element.innerHTML;
+
+console.log("sonnet", sonnet);
+
+var element = orphans;
+console.log("starting position of orphans: ", quote.indexOf("orphans"));
+
+
+
+console.log("sonnet: ", quote.length);

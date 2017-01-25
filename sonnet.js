d4080578cceb11d429f572c4a3730ae36c53fa91
ellisthomas <ellisthomas@gmail.com>
2017-01-24 19:00:43 -0600

    sonnet js

--- sonnet.js
+++ sonnet.js
@@ -1,18 +1,14 @@
 var element = document.getElementById("sonnet");
-console.log(element);
 var sonnet = element.innerHTML;
 
 var indexOfOrphans = sonnet.indexOf("orphans");
-console.log(indexOfOrphans);
 
 alert("There are " + sonnet.length +" characters in this sonnet");
 
 var textreplace = sonnet.replace("winter","yuletide");
-console.log(textreplace);
 
-var globalreplace = sonnet.replace(/the/g,"a large");
-console.log(globalreplace);
+var globalreplace = sonnet.replace(/the /g," a large");
+
+element.innerHTML = globalreplace;
 
 
-document.getElementById("sonnet").innerHTML = "When your brain is fried from JavaScript!"
-console.log(sonnet);

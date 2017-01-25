36bd1d227123c96afdc67fd9c4dbb9be0e0ffa17
ellisthomas <ellisthomas@gmail.com>
2017-01-24 19:22:26 -0600

    finished switch

---
+++ day2.js
@@ -0,0 +1,13 @@
+// switch statement
+
+var day = "Friday";
+
+switch(day){
+	case "Monday":
+	console.log("Boooooo");
+	case "Friday":
+	console.log("PARTY")
+	break;
+	default:
+	console.log("not monday")
+}
